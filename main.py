e51f9cc804e1b0ada5ffecf6d5d0717ede8ad6d6
blackrookgames <blackrookgames@gmail.com>
2026-02-11 20:01:28 -0500

    Added buy and sell functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ------------------------------------------------------------------------->
 import pathlib
 import sys
 
@@ -6,6 +7,10 @@ import src
 def main():
     try:
         if len(sys.argv) == 0: return 1
+        # What to do?
+        whattodo = '0'
+        if len(sys.argv) > 1:
+            whattodo = sys.argv[1]
         # Get directories
         pydir = pathlib.Path(sys.argv[0]).resolve().parent
         secretdir = pydir.joinpath("secret")
@@ -14,11 +19,23 @@ def main():
         api = src.APIInfo(apipath)
         # Create crypto interface
         cry = src.Cry(api)
-        balance = cry.get_balance('USD')
-        # order = cry.order_buy('BTC/USD', 5)
-# ------------------------------------------------------------------------->
-        
-        print(f"USD Balance: {balance}")
+        # Do what to do
+        match whattodo:
+            case '0':
+                balance = cry.get_balance('USD')
+                print(f"Balance: {balance} USD")
+            case '1':
+                amount = 5
+                markets = cry.load_markets()
+                order = cry.order_buy('BTC/USD', amount)
+                print(f"Bought {amount} USD worth of BTC")
+            case '2':
+                amount = 5
+                order = cry.order_sell('BTC/USD',\
+                    cry.compute_from('BTC/USD', amount))
+                print(f"Sold {amount} USD worth of BTC")
+            case _:
+                raise src.CLIError(f"Invalid code: {whattodo}")
     except src.CLIError as _e:
         print(f"ERROR: {_e}", file = sys.stderr)
         return 1
--- src/c_Cry.py
+++ src/c_Cry.py
@@ -2,6 +2,9 @@ all = ['Cry']
 
 import ccxt as _ccxt
 
+from typing import\
+    cast as _cast
+
 from .c_APIInfo import\
     APIInfo as _APIInfo
 from .c_CLIError import\
@@ -28,6 +31,14 @@ class Cry:
                 'apiKey': api.key, \
                 'secret': api.secret, \
                 'enableRateLimit': True, })
+            # Options
+            if not isinstance(self.__exchange.options, dict):
+                raise _CLIError("Failed to create interface.")
+            option = 'createMarketSellOrderRequiresPrice'
+            self.__exchange.options[option] = 'False' # type: ignore
+            option = 'createMarketBuyOrderRequiresPrice'
+            self.__exchange.options[option] = 'False' # type: ignore
+            # Success!!!
             return
         except Exception as _e:
             e = _CLIError(_e)
@@ -37,6 +48,23 @@ class Cry:
 
     #region method
 
+    def load_markets(self, reload:bool = False):
+        """
+        Loads the markets
+
+        :param reload:
+            Whether or not to force a reload of the markets
+        :return:
+            Dictionary of markets
+        :raise CLIError:
+            An error occurred
+        """
+        try:
+            return self.__exchange.load_markets(reload = reload)
+        except Exception as _e:
+            e = _CLIError(_e)
+        raise e
+        
     def get_balance(self, currency:str):
         """
         Gets the total for balance for the specified currency
@@ -54,61 +82,136 @@ class Cry:
             e = _CLIError(_e)
         raise e
 
+    def get_price(self, symbol:str):
+        """
+        Gets a price for a pair\n
+        Example: For the pair BTC/USD, this will fetch the BTC price.
+        
+        :param symbol:
+            Symbol (ex: 'BTC/USD')
+        :param amount:
+            Amount
+        :return:
+            Price
+        :raise CLIError:
+            An error occurred
+        """
+        try:
+            ticker = self.__exchange.fetch_ticker(symbol)
+            return float(ticker['last']) # type: ignore
+        except Exception as _e:
+            e = _CLIError(_e)
+        raise e
+    
+    def compute_to(self, symbol:str, amount:float):
+        """
+        Computes an amount using a pair\n
+        Example: For the pair BTC/USD, this will compute the USD equivalent 
+        of a BTC amount.
+        
+        :param symbol:
+            Symbol (ex: 'BTC/USD')
+        :param amount:
+            Input amount
+        :return:
+            Computed amount
+        :raise CLIError:
+            An error occurred
+        """
+        try:
+            return amount * self.get_price(symbol)
+        except Exception as _e:
+            e = _CLIError(_e)
+        raise e
+    
+    def compute_from(self, symbol:str, amount:float):
+        """
+        Computes an amount using a pair\n
+        Example: For the pair BTC/USD, this will compute the BTC equivalent 
+        of a USD amount.
+        
+        :param symbol:
+            Symbol (ex: 'BTC/USD')
+        :param amount:
+            Input amount
+        :return:
+            Computed amount
+        :raise CLIError:
+            An error occurred
+        """
+        try:
+            return amount / self.get_price(symbol)
+        except Exception as _e:
+            e = _CLIError(_e)
+        raise e
+
     def order_buy(self, symbol:str, amount:float):
-        """
-        Creates a buy order
-        
-        :param symbol:
-            Symbol (ex: 'BTC/USD')
-        :param amount:
-            Amount
-        :return:
-            Order ID
-        :raise CLIError:
-            An error occurred
-        """
-        try:
-            amount = self.__exchange.amount_to_precision(\
-                symbol, amount) # type: ignore
-            orderid = self.__exchange.create_market_buy_order(\
-                symbol, amount) # type: ignore
-            return orderid
-        except Exception as _e:
-            e = _CLIError(_e)
-        raise e
-
+            """
+            Creates a buy order
+            
+            :param symbol:
+                Symbol (ex: 'BTC/USD')
+            :param amount:
+                Amount (ex: for 'BTC/USD' this is the USD amount)
+            :return:
+                order structure <https://docs.ccxt.com/?id=order-structure>
+            :raise CLIError:
+                An error occurred
+            """
+            try:
+                amount = \
+                    self.__exchange.amount_to_precision(\
+                    symbol, amount) # type: ignore
+                orderid = \
+                    self.__exchange.create_market_buy_order_with_cost(\
+                    symbol, amount) # type: ignore
+                return orderid
+            except Exception as _e:
+                e = _CLIError(_e)
+            raise e
+    
     def order_sell(self, symbol:str, amount:float):
-        """
-        Creates a sell order
-        
-        :param symbol:
-            Symbol (ex: 'BTC/USD')
-        :param amount:
-            Amount
-        :return:
-            Order ID
-        :raise CLIError:
-            An error occurred
-        """
-        try:
-            amount = self.__exchange.amount_to_precision(\
-                symbol, amount) # type: ignore
-            orderid = self.__exchange.create_market_sell_order(\
-                symbol, amount) # type: ignore
-            return orderid
-        except Exception as _e:
-            e = _CLIError(_e)
-        raise e
+            """
+            Creates a sell order
+            
+            :param symbol:
+                Symbol (ex: 'BTC/USD')
+            :param amount:
+                Amount (ex: for 'BTC/USD' this is the BTC amount)
+            :return:
+                order structure <https://docs.ccxt.com/?id=order-structure>
+            :raise CLIError:
+                An error occurred
+            """
+            try:
+                amount = \
+                    self.__exchange.amount_to_precision(\
+                    symbol, amount) # type: ignore
+                orderid = \
+                    self.__exchange.create_market_sell_order_with_cost(\
+                    symbol, amount) # type: ignore
+                return orderid
+            except Exception as _e:
+                e = _CLIError(_e)
+            raise e
 
-    def order_status(self, orderid, symbol:str):
+    def order_get(self, orderid:str, symbol:str):
         """
-        Retrieves the status of an order
+        Retrieves an order with the specified ID
         
         :param orderid:
             Order ID
         :param symbol:
             Symbol (ex: 'BTC/USD')
+        :return:
+            order structure <https://docs.ccxt.com/?id=order-structure>
+        :raise CLIError:
+            An error occurred
         """
-        self.__exchange.fetch_order(orderid, symbol)
+        try:
+            return self.__exchange.fetch_order(orderid, symbol)
+        except Exception as _e:
+            e = _CLIError(_e)
+        raise e
 
     #endregion
